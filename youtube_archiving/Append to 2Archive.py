4ecc23b604b9da687ecf347068e1734517421e7e
cclauss <cclauss@bluewin.ch>
2016-04-29 01:16:34 +0200

    Minor optimizations
    https://realpython.com/blog/python/the-most-diabolical-python-antipattern/
    
    https://docs.python.org/2/library/functions.html?highlight=input#input

--- youtube_archiving/Append to 2Archive.py
+++ youtube_archiving/Append to 2Archive.py
@@ -22,45 +22,41 @@ def title_of_url(url):
   try:
     soup = BeautifulSoup(urllib2.urlopen(url))
     return soup.title.string
-u  except:
+  except:  # caution: naked exception
     return ''
 
 
 guid = '__YOUR_NOTE_GUID_HERE__'
 
-input = ''
+user_input = ''  # input is a built-in function so use a different name
 
 if appex.is_running_extension():
 	LKEvernoteApi.log_progress('load url provided to app extension')
-	input = appex.get_url()
+	user_input = appex.get_url()
 else:
 	LKEvernoteApi.log_progress('not running from extension, checking arguments')
 	if len(sys.argv) > 1:
 		evernote.log_progress('argument found, use that')
-		input = sys.argv[1]
+		user_input = sys.argv[1]
 	else:
 		LKEvernoteApi.log_progress('no arguments found, will use clipboard text')
-		input = clipboard.get()
-		if clipboard.get() == '':
+		user_input = clipboard.get()
+		if not user_input:
 			sys.exit('Clipboard is empty, no arguments passed to script')
 
 LKEvernoteApi.log_progress('Loading title of passed url')
-url_title = ' (' + title_of_url(input) + ') '
-
-if url_title is ' () ':
-	url_title = ''
-else:
-	url_title = url_title.replace('&', 'and')
-
-LKEvernoteApi.log_progress('create date text')
-date = datetime.datetime.now().strftime('%d %b %Y %H:%M')
+url_title = title_of_url(user_input)
+if url_title:
+	url_title = ' ({}) '.format(url_title.replace('&', 'and'))
 
 LKEvernoteApi.log_progress('create ENML string')
-en_todo_text = '<en-todo checked="false"></en-todo> {0}{1}(@ {2})'.format(input, url_title, date)
+fmt = '<en-todo checked="false"></en-todo> {}{}(@ {:%d %b %Y %H:%M})'
+en_todo_text = fmt.format(input, url_title, datetime.datetime.now())
 print(en_todo_text)
 
 LKEvernoteApi.log_progress('call ´appendNote´ function')
-LKEvernoteApi.append_to_note(guid=guid, new_content=en_todo_text, main_new_content=input, add_if_already_exists=add_if_already_exists)
+LKEvernoteApi.append_to_note(guid=guid, new_content=en_todo_text, main_new_content=user_input,
+			     add_if_already_exists=add_if_already_exists)
 
 LKEvernoteApi.log_progress('Done')
 
